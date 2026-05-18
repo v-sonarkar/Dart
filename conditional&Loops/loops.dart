d72e145e6f2e15eadb1c67ed8fd42002b968909a
Sujal Sonarkar <sonarkarsujal4@gmail.com>
2026-05-18 23:24:35 +0530

    Add conditional&Loops/loops.dart

---
+++ conditional&Loops/loops.dart
@@ -0,0 +1,22 @@
+void main(){
+
+
+  // For loop
+  for (int i = 0; i < 5; i++) {
+    print("For loop iteration: $i");
+  }
+
+  // While loop
+  int j = 0;
+  while (j < 5) {
+    print("While loop iteration: $j");
+    j++;
+  }
+
+  // Do-while loop
+  int k = 0;
+  do {
+    print("Do-while loop iteration: $k");
+    k++;
+  } while (k < 5);
+}
